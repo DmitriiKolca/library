7183a6be7531a192378b5c1fc215b64e79aa8638
Dmitrii Kolca <leonidi4@list.ru>
2024-12-09 11:24:51 +0200

    First Lib commit

---
+++ src/lib.ts
@@ -0,0 +1,4 @@
+export const variable1 = 12345;
+export const variable2 = 'Hello world';
+export const variable3 = [12,13,14,15];
+export const variable4 = ['Hello world 1','Hello world 2','Hello world 3','Hello world 4'];
