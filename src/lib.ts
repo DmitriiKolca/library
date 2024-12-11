f0ffd4d5a46b4ff8ff2c56cc4624341927000a74
Dmitrii Kolca <leonidi4@list.ru>
2024-12-11 11:55:56 +0200

    Add variable 6 from the main project

--- src/lib.ts
+++ src/lib.ts
@@ -3,3 +3,4 @@ export const variable2 = 'Hello world';
 export const variable3 = 'New Value of variable';
 export const variable4 = ['Hello world 1','Hello world 2','Hello world 3','Hello world 4'];
 export const variable5 = "Add variable from main project"
+export const variable6 = "Test add variable from main project"
